2fb5764be47b3abce2c8f5fc5e78ff475e239154
Pedro Cabral <pecabralsantos@gmail.com>
2021-09-27 15:09:25 -0300

    Configurações iniciais.

---
+++ Contatos/app/src/main/java/br/com/multitrek/contatos/MainActivity.kt
@@ -0,0 +1,18 @@
+package br.com.multitrek.contatos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.multitrek.contatos.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
